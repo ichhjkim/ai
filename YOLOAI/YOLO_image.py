2120acc14e59e6feddc562aee66b2c4a2b510f72
ichhjkim <nillcafri123@gmail.com>
2020-04-30 21:08:00 +0900

    파일경로 수정

--- YOLOAI/YOLO_image.py
+++ YOLOAI/YOLO_image.py
@@ -5,7 +5,7 @@ import os
 def YOLO(image):
     # net = Detector(bytes("cfg/densenet201.cfg", encoding="utf-8"), bytes("densenet201.weights", encoding="utf-8"), 0, bytes("cfg/imagenet1k.data",encoding="utf-8"))
 
-    net = Detector(bytes("cfg/yolov3_sub3", encoding="utf-8"), bytes("weights/yolov3_sub3.weights", encoding="utf-8"), 0, bytes("cfg/obj.data",encoding="utf-8"))
+    net = Detector(bytes(os.path.join("cfg/yolov3_sub3"), encoding="utf-8"), bytes(os.path.join("weights/yolov3_sub3.weights"), encoding="utf-8"), 0, bytes(os.path.join("cfg/obj.data"),encoding="utf-8"))
     print(os.path.abspath(__file__), 'yolo image 경로')
     img = cv2.imread(image)
 
